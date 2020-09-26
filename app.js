8ee4621ad692951eff1f832d609e19c319aeb13b
rohgithub <rohit.jnvmj@gmail.com>
2020-09-26 19:34:35 +0530

    Final

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const morgan= require('morgan');
+
+
+const orderRoutes= require('./api/routes/orders');
+const productRoutes=require('./api/routes/products');
+
+app.use(morgan('dev'));
+
+app.use('/products',productRoutes);
+app.use('/products',orderRoutes);
+
+app.use((req,res,next)=> {
+ const error = new Error('Not Found');
+ error.status=404;
+ next(error);
+
+
+});
+
+app.use((error,req,res,next)=> {
+    res.status(error.status||500);
+    res.json({
+
+        error:{
+            message: error.message
+        }
+
+
+
+    });
+   
+   
+   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
